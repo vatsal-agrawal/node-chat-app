c7ea7aa4da7669859f1ea6ea571ad207ed943308
vatsal-agrawal <vatsal.agarwal.54@gmail.com>
2018-03-20 20:29:52 +0530

    Event emmiter and listener

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const path = require('path')
+const http = require('http');
+const socketIO = require('socket.io');
 
 var pathJoin = path.join(__dirname,'../public')
 var app = express();
 
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(pathJoin));
 
-app.listen('3000',()=>{
+
+io.on('connection',(socket)=>{
+    console.log('user connected');
+    socket.on('disconnect',()=>{
+        console.log('user disconnected');
+    });  
+})
+
+server.listen('3000',()=>{
     console.log('Server is up and Running');
     
 })
